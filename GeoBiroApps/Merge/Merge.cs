271f69db5b972613add86f2d6730eca4ff6c0fca
Haris Ćesir <harecesir@gmail.com>
2018-09-14 16:22:10 +0200

    i

--- GeoBiroApps/Merge/Merge.cs
+++ GeoBiroApps/Merge/Merge.cs
@@ -18,6 +18,7 @@ namespace GeoBiroApps.Merge
         List<string> path = new List<string>();
         string SacuvanTxt;
         string Obavijest = " ";
+        List<string> Lista = new List<string>();
         public Merge()
         {
             InitializeComponent();
@@ -56,58 +57,84 @@ namespace GeoBiroApps.Merge
 
 
             }
-            int broj = 1;
-            foreach (string a in path)
-            {
-
-                using (var tw = new StreamWriter(saveDialog.FileName, true))
+          
+                foreach (string a in path)
                 {
+                    int BrojLinije = 1;
                     string line;
                     System.IO.StreamReader file =
                      new System.IO.StreamReader(a);
                     while ((line = file.ReadLine()) != null)
                     {
+                        if (!string.IsNullOrEmpty(line))
+                        {
 
-                        if (line.Substring(line.Length - 3) == "POL")
-                        {
-                            tw.WriteLine(line);
+                                try
+                                {
+                                     Lista.Add(line);
+                                     BrojLinije++;
+                                  
+                                }
+                                catch
+                                {
+
+                                 Obavijest = Obavijest + "Greška u txt fajlu: " + a + " Na liniji:  " + BrojLinije;
+                                 break;
+
+                                }
+
+                            }
+
                         }
-                        else
+                file.Close();
+            }
+
+            if (Obavijest == " ")
+            {
+                try
+                {
+                    using (var tw = new StreamWriter(saveDialog.FileName, true))
+                    {
+                        int broj = 1;
+                        foreach (string t in Lista)
                         {
-                            try
+                            if (t.Substring(t.Length - 3) == "POL")
                             {
-                                line = line.Replace(line.Split(',')[0], broj.ToString());
-                                tw.WriteLine(line);
+                                tw.WriteLine(t);
+                            }
+                            else
+                            {
+                                string k = broj + "," + t.Substring(t.IndexOf(',') + 1);
+
+                                tw.WriteLine(k);
                                 broj++;
                             }
-                            catch
-                            {
-                                Obavijest = Obavijest + "Greška u txt fajlu";
-                               
-                                
-                            }
-
                         }
+
 
                     }
 
-                    file.Close();
-
-
-
+                    MessageBox.Show("Završeno");
                 }
-
-
-
-            }
-            if (Obavijest == " ")
-            {
-                MessageBox.Show("Završeno");
+                catch
+                {
+                    MessageBox.Show("Dogodila se greska prilikom pisanja obrasca molimo pokušajte ponovo");
+                   
+                }
+               
             }
             else
             {
                 MessageBox.Show(Obavijest);
             }
+
+            
+                
+
+                
+
+            
+           
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -142,87 +169,99 @@ namespace GeoBiroApps.Merge
 
 
 
-
-
-
-            int zadnjiROwO25 = 6;
-            int zadnjiROwTacke = 5;
-            int broj = 1;
-            string line;
-            System.IO.StreamReader file =
-             new System.IO.StreamReader(SacuvanTxt);
-            while ((line = file.ReadLine()) != null)
+            try
             {
 
-                if (line.Substring(line.Length - 3) == "POL")
+
+                int zadnjiROwO25 = 6;
+                int zadnjiROwTacke = 5;
+                int broj = 1;
+                string line;
+                System.IO.StreamReader file =
+                 new System.IO.StreamReader(SacuvanTxt);
+                while ((line = file.ReadLine()) != null)
                 {
 
-                    Microsoft.Office.Interop.Excel.Worksheet O25 = xlsWorkBook.Worksheets[1];
-                    O25 = xlsWorkBook.Sheets[1];
-                    O25.Select(true);
+                    if (line.Substring(line.Length - 3) == "POL")
+                    {
 
-                    O25.Cells[zadnjiROwO25, 1] = line.Split(',')[0];
-                    O25.Cells[zadnjiROwO25, 3] = line.Split(',')[1];
-                    O25.Cells[zadnjiROwO25, 4] = line.Split(',')[2];
-                    O25.Cells[zadnjiROwO25, 5] = line.Split(',')[3];
-                    zadnjiROwO25++;
+                        Microsoft.Office.Interop.Excel.Worksheet O25 = xlsWorkBook.Worksheets[1];
+                        O25 = xlsWorkBook.Sheets[1];
+                        O25.Select(true);
 
-                    Microsoft.Office.Interop.Excel.Worksheet Tacke = xlsWorkBook.Worksheets[2];
-                    Tacke = xlsWorkBook.Sheets[2];
-                    Tacke.Select(true);
+                        O25.Cells[zadnjiROwO25, 1] = line.Split(',')[0];
+                        O25.Cells[zadnjiROwO25, 3] = line.Split(',')[1];
+                        O25.Cells[zadnjiROwO25, 4] = line.Split(',')[2];
+                        O25.Cells[zadnjiROwO25, 5] = line.Split(',')[3];
+                        zadnjiROwO25++;
 
-                    Tacke.Cells[zadnjiROwTacke, 1] = broj;
-                    Tacke.Cells[zadnjiROwTacke, 2] = line.Split(',')[0];
-                    Tacke.Cells[zadnjiROwTacke, 3] = "POL";
-                    Tacke.Cells[zadnjiROwTacke, 4] = line.Split(',')[1];
-                    Tacke.Cells[zadnjiROwTacke, 5] = line.Split(',')[2];
-                    Tacke.Cells[zadnjiROwTacke, 6] = line.Split(',')[3];
-                    broj++;
-                    zadnjiROwTacke++;
+                        Microsoft.Office.Interop.Excel.Worksheet Tacke = xlsWorkBook.Worksheets[2];
+                        Tacke = xlsWorkBook.Sheets[2];
+                        Tacke.Select(true);
+
+                        Tacke.Cells[zadnjiROwTacke, 1] = broj;
+                        Tacke.Cells[zadnjiROwTacke, 2] = line.Split(',')[0];
+                        Tacke.Cells[zadnjiROwTacke, 3] = "POL";
+                        Tacke.Cells[zadnjiROwTacke, 4] = line.Split(',')[1];
+                        Tacke.Cells[zadnjiROwTacke, 5] = line.Split(',')[2];
+                        Tacke.Cells[zadnjiROwTacke, 6] = line.Split(',')[3];
+                        broj++;
+                        zadnjiROwTacke++;
+                    }
+                    else
+                    {
+                        Microsoft.Office.Interop.Excel.Worksheet Tacke = xlsWorkBook.Worksheets[2];
+                        Tacke = xlsWorkBook.Sheets[2];
+                        Tacke.Select(true);
+                        Tacke.Cells[zadnjiROwTacke, 1] = broj;
+                        Tacke.Cells[zadnjiROwTacke, 2] = line.Split(',')[0];
+
+                        Tacke.Cells[zadnjiROwTacke, 4] = line.Split(',')[1];
+                        Tacke.Cells[zadnjiROwTacke, 5] = line.Split(',')[2];
+                        Tacke.Cells[zadnjiROwTacke, 6] = line.Split(',')[3];
+                        broj++;
+                        zadnjiROwTacke++;
+
+                    }
+
                 }
-                else
+
+                file.Close();
+
+                SaveFileDialog saveDialog = new SaveFileDialog();
+                saveDialog.Filter = "Excel files (*.xlsx, *.xls)|*.xlsx; *.xls";
+                saveDialog.FilterIndex = 2;
+                saveDialog.FileName = "Excel.xlsx";
+
+                if (saveDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
                 {
+
                     Microsoft.Office.Interop.Excel.Worksheet Tacke = xlsWorkBook.Worksheets[2];
-                    Tacke = xlsWorkBook.Sheets[2];
-                    Tacke.Select(true);
-                    Tacke.Cells[zadnjiROwTacke, 1] = broj;
-                    Tacke.Cells[zadnjiROwTacke, 2] = line.Split(',')[0];
 
-                    Tacke.Cells[zadnjiROwTacke, 4] = line.Split(',')[1];
-                    Tacke.Cells[zadnjiROwTacke, 5] = line.Split(',')[2];
-                    Tacke.Cells[zadnjiROwTacke, 6] = line.Split(',')[3];
-                    broj++;
-                    zadnjiROwTacke++;
+                    Tacke.SaveAs(saveDialog.FileName);
 
+                    //  Microsoft.Office.Interop.Excel.Worksheet O25 = xlsWorkBook.Worksheets[1];
+                    // O25.SaveAs(saveDialog.FileName);
+                    MessageBox.Show("Zavrseno", "Informacija");
                 }
+                xlsWorkBook.Close(true);
 
+                xls.Quit();
+
+                Marshal.ReleaseComObject(xlsWorkBook);
+                //Marshal.ReleaseComObject(Tacke);
+                Marshal.ReleaseComObject(xls);
             }
-
-            file.Close();
-
-            SaveFileDialog saveDialog = new SaveFileDialog();
-            saveDialog.Filter = "Excel files (*.xlsx, *.xls)|*.xlsx; *.xls";
-            saveDialog.FilterIndex = 2;
-            saveDialog.FileName = "Excel.xlsx";
-
-            if (saveDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            catch
             {
+                xlsWorkBook.Close(true);
 
-                Microsoft.Office.Interop.Excel.Worksheet Tacke = xlsWorkBook.Worksheets[2];
+                xls.Quit();
 
-                Tacke.SaveAs(saveDialog.FileName);
-
-                //  Microsoft.Office.Interop.Excel.Worksheet O25 = xlsWorkBook.Worksheets[1];
-                // O25.SaveAs(saveDialog.FileName);
-                MessageBox.Show("Zavrseno", "Informacija");
+                Marshal.ReleaseComObject(xlsWorkBook);
+                //Marshal.ReleaseComObject(Tacke);
+                Marshal.ReleaseComObject(xls);
             }
-            xlsWorkBook.Close(true);
-
-            xls.Quit();
-
-            Marshal.ReleaseComObject(xlsWorkBook);
-            //Marshal.ReleaseComObject(Tacke);
-            Marshal.ReleaseComObject(xls);
         }
 
         private void button4_Click(object sender, EventArgs e)
